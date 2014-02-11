b05e21aaf17b20982e5edd6a25533352150208d2
Jasper Tomas <jaspertomas@gmail.com>
2014-02-11 15:25:30 +0800

    created file writing mechanism

--- src/mysqlmodelgenerator/MysqlModelGenerator.java
+++ src/mysqlmodelgenerator/MysqlModelGenerator.java
@@ -4,15 +4,14 @@
  */
 package mysqlmodelgenerator;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import models.User;
+import java.util.ArrayList;
 import utils.MySqlDBHelper;
+import utils.fileaccess.FileWriter;
 
 /**
  *
@@ -32,17 +31,39 @@ public class MysqlModelGenerator {
         
         Statement st = null;
         ResultSet rs = null;
+        ArrayList<String> tables=new ArrayList<String>();
+        ArrayList<String> fields=new ArrayList<String>();
+        ArrayList<String> fieldtypes=new ArrayList<String>();
         try { 
             st = conn.createStatement();
-//                rs = st.executeQuery("SELECT VERSION()");
             rs = st.executeQuery("Show Tables");
 
             while (rs.next()) {
-                System.out.println(rs.getString(1));
+                tables.add(rs.getString(1));
+            }
+            for(String table:tables) {
+                rs = st.executeQuery("SHOW COLUMNS FROM "+table);
+                fields.clear();
+                fieldtypes.clear();
+                while (rs.next()) {
+                    fields.add(rs.getString(1));
+                    fieldtypes.add(rs.getString(2));
+                }
+                createModelFile(table,fields,fieldtypes);
             }
         } catch (SQLException ex) {
-            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
             ex.printStackTrace();
         }
+    }
+
+    public static void createModelFile(String table, ArrayList<String> fields, ArrayList<String> fieldtypes)
+    {
+        //write file to current directory
+        File outputdir=new File("./output");
+        outputdir.mkdir();
+        FileWriter.write(outputdir.getPath()+"/a.txt", "hello world");
+
+        
     }
 }
